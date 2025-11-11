aef66abe3791fe1ebceb2c9b3ca4c15e358e750a
KimMinWoo <lucif040124@gmail.com>
2025-11-11 18:59:57 +0900

    회원가입페이지 최종완성

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth import password_validation # ⬅️ 1. 이 줄을 추가합니다.
 
 class SignUpForm(UserCreationForm):
     # '이름' 필드
@@ -53,6 +54,44 @@ class SignUpForm(UserCreationForm):
             if User.objects.filter(username=email).exists():
                 raise forms.ValidationError("이미 가입된 이메일입니다.")
         return email
+
+    #비밀번호 검사관련 코드
+    def clean(self):
+        cleaned_data = super(UserCreationForm, self).clean()
+
+        password = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password and password2 and password != password2:
+            self.add_error('password2', forms.ValidationError("두 비밀번호가 일치하지 않습니다."))
+        
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except forms.ValidationError as error:
+                korean_errors = []
+                for e in error.error_list:
+                    if e.code == 'password_too_short':
+                        korean_errors.append(forms.ValidationError(
+                            f"비밀번호는 최소 {e.params.get('min_length', 8)}자 이상이어야 합니다.",
+                            code=e.code
+                        ))
+                    elif e.code == 'password_common':
+                        korean_errors.append(forms.ValidationError(
+                            "비밀번호가 너무 일상적입니다.",
+                            code=e.code
+                        ))
+                    elif e.code == 'password_entirely_numeric':
+                        korean_errors.append(forms.ValidationError(
+                            "비밀번호는 숫자로만 구성될 수 없습니다.",
+                            code=e.code
+                        ))
+                    else:
+                        korean_errors.append(e)
+                    
+                self.add_error('password1', korean_errors)
+
+        return cleaned_data
 
     def save(self, commit=True):
         user = super().save(commit=False)
